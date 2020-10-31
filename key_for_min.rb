83c73d1e502e5dd0ceb163c84b2edfe295a8c06e
Mark G <markghaida@gmail.com>
2020-10-31 13:07:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
+
  def key_for_min_value(name_hash)
   num = 0
   name = nil
